95c8a5b82cb58b2d575e4ce500e1432ec20f1285
halfmoon-mind <5338095@gmail.com>
2026-03-30 02:01:27 +0900

    refactor: remove Schedule module, all functionality now in Performance

--- apps/server/src/schedule/dto/update-schedule.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType, OmitType } from '@nestjs/swagger';
-import { CreateScheduleDto } from './create-schedule.dto';
-
-export class UpdateScheduleDto extends PartialType(
-  OmitType(CreateScheduleDto, ['artistId'] as const),
-) {}
